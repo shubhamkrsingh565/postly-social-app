b0a4ed34d1b9a29a31136947475b70bd2aa6e173
shubhamkrsingh565 <shubhamkrsingh565@gmail.com>
2025-08-08 18:06:31 +0530

    shadcn setup  navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="">
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="m-4">
+      <h1>Home page view</h1>
     </div>
   );
 };
