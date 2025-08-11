715df5c856687002de2bb4f046bca74ea2a2c36b
shubhamkrsingh565 <shubhamkrsingh565@gmail.com>
2025-08-11 13:04:26 +0530

    Build sidebar, posts, like, comment, delete, suggest users and notification page

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from "@/actions/profile.action";
+
+export async function generateMetadata({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
+
+async function ProfilePageServer({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
